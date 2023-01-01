bb8861630c73bffbd24188e1a4c6aabe34bd81d7
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-01-01 12:11:09 +0530

    First Commit

--- app/src/main/java/com/example/whatsthenews/MainActivity.kt
+++ app/src/main/java/com/example/whatsthenews/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.whatsthenews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//API Key : a629c54bbc614f9a8735ec1bd7efbe85
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
