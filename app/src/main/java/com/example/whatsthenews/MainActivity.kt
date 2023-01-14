0722c1a4b6f55fde6cb9adfe674503212a793a31
SajalRuhela017 <ruhela.sajal017@gmail.com>
2023-01-15 01:49:06 +0530

    First Commit

--- app/src/main/java/com/example/whatsthenews/MainActivity.kt
+++ app/src/main/java/com/example/whatsthenews/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.whatsthenews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.whatsthenews.databinding.ActivityMainBinding
 
 //API Key : a629c54bbc614f9a8735ec1bd7efbe85
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        var binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
